eb8a727428434c8439aae06175b74ecca2e64135
Fhjorth <43441370+Fhjorth@users.noreply.github.com>
2021-02-19 19:19:26 +0100

    init commit

---
+++ MeterLog/MeterLog/MeterLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeterLogApp.swift
+//  MeterLog
+//
+//  Created by Frederik Hjorth on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MeterLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
