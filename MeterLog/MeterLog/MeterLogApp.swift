f710e399771d482924b416cf01cb2ea3986342d9
Henrik Top Mygind <htmy@bang-olufsen.dk>
2021-02-21 13:05:34 +0100

    cleanup

--- MeterLog/MeterLog/MeterLogApp.swift
+++ MeterLog/MeterLog/MeterLogApp.swift
@@ -27,19 +27,3 @@ struct MeterLogApp: App {
         }
     }
 }
-
-//import UIKit
-//import Firebase
-//
-//@UIApplicationMain
-//class AppDelegate: UIResponder, UIApplicationDelegate {
-//
-//  var window: UIWindow?
-//
-//  func application(_ application: UIApplication,
-//    didFinishLaunchingWithOptions launchOptions:
-//      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-//    FirebaseApp.configure()
-//    return true
-//  }
-//}
