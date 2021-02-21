99ba69b682fe98d55c4bf98db285f498edfe0f53
Henrik Top Mygind <htmy@bang-olufsen.dk>
2021-02-21 20:05:18 +0100

    anti revers

--- MeterLog/MeterLog/Managers/Car.swift
+++ MeterLog/MeterLog/Managers/Car.swift
@@ -24,14 +24,25 @@ class Car: ObservableObject, Identifiable {
             odometers = fillups.map { f in
                 Double(f.odometer)
             }
+            
+            var totalPrices = Float(0)
+            fillups.forEach { f in
+                totalPrices += f.volume * f.literPrice
+            }
+            self.totalPrices = totalPrices
+            
+            
         }
     }
     
     @Published var odometers = [Double]()
     
+    @Published var totalPrices = Float()
+    
+    
     func addNewFillup(_ fillup: Fillup){
         fillups.append(fillup)
-
+        
         guard let database = database else { return }
         database.addFillup(fillup, for: self)
     }
@@ -63,4 +74,5 @@ class Car: ObservableObject, Identifiable {
         ])
         return testCar
     }()
+    
 }
